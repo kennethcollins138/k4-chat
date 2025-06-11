48fdc640ae9e8a58b53cb630e878df90ee8f1e28
kennethcollins138 <kennethcollinswork@gmail.com>
2025-06-11 16:41:49 -0700

    [feature/api/auth-endpont]: Added quick brainstorm about actor integration

--- backend/pkg/api/routes.go
+++ backend/pkg/api/routes.go
@@ -34,6 +34,10 @@ func (s *Server) RegisterRoutes(router chi.Router) {
 	// Initialize Auth middleware
 	authMiddleware := middleware.NewAuthMiddleware(sessionManager, nil, tokenStore, s.pg, s.redis, s.logger)
 	// Initialize actor managers/supervisor
+	// TODO: Potentially think about passing Supervisor actor as global to service layer.
+	// Share PIDs keep services as main business logic for handling req, but offload lot of handling to actors.
+	// EXAMPLE: user signs in, I create a UserActor through useractormanager, keeps the buckets and sorts the user actor to necessary bucket,
+	// UserActor/Cache is warm and ready for a ws connection, if user's sessions are still valid, but user isn't active tear down instance until they are active again(eg on Token Refresh)
 
 	// Initialize auth service and handler
 	authRepo := auth.NewRepository(s.pg, s.logger)
